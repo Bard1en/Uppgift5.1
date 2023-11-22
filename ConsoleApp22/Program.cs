369bc3373f1aceade5d99f897e972c8c8f574c2e
Bard1en <greger.grym@gmail.com>
2023-11-22 10:03:45 +0100

    Add project files.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace uppgift5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in 5 namn");
+            string[] namn = new string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                namn[i] = Console.ReadLine();
+            }  
+            for(int a = 0; a < 5; a++)
+            {
+                Console.WriteLine(namn[a]);
+            }
+            
+        }
+    }
+}
+
